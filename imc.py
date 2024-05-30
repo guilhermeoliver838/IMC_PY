ccbabf8c44d9f2165b56aaa21bcd60cad69ad931
Guilherme <guilhermeoliver838@gmail.com>
2024-05-30 10:32:53 -0300

    Primeiro commit

---
+++ imc.py
@@ -0,0 +1,16 @@
+#Declaração das variáveis
+peso = float
+altura = float
+imc = float
+
+#Entrada de dados Peso e Altura
+peso = float(input("Insira seu peso: "))
+altura = float(input("Insira sua altura: "))
+
+#Calculo para o IMC
+imc = peso/altura
+
+#Exibição do Resultado do Calculo do IMC
+print("Seu IMC é: ", imc)
+
+#Guilherme de Carvalho Oliveira - RA: 3578293703
