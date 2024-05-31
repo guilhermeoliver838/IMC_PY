7a9e752c63587375147552faa38f0a9b461e5737
Guilherme <guilhermeoliver838@gmail.com>
2024-05-31 11:35:47 -0300

    Atualização da calculadora de IMC

--- imc.py
+++ imc.py
@@ -8,9 +8,15 @@ peso = float(input("Insira seu peso: "))
 altura = float(input("Insira sua altura: "))
 
 #Calculo para o IMC
-imc = peso/altura
+imc = peso/(altura ** 2)
 
 #Exibição do Resultado do Calculo do IMC
 print("Seu IMC é: ", imc)
 
-#Guilherme de Carvalho Oliveira - RA: 3578293703
+if imc >= 20:
+    print("Tá igual a Moby Dyck")
+
+elif imc <= 20:
+    print("Olha a Olivia Palito")
+
+#Guilherme de Carvalho Oliveira -
